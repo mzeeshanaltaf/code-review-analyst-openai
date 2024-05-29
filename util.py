a4c41b083cca6c409de13669c4de8d840fb7b761
mzeeshanaltaf <154883001+mzeeshanaltaf@users.noreply.github.com>
2024-05-29 11:25:13 +0500

    Add files via upload

--- util.py
+++ util.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import time
-import base64
 import json
 
 
@@ -26,19 +24,3 @@ def configure_apikey_sidebar():
 
     return api_key, is_active
 
-
-# Function to download text content as a file using Streamlit
-def text_downloader(raw_text):
-    # Generate a timestamp for the filename to ensure uniqueness
-    time_str = time.strftime("%Y%m%d-%H%M%S")
-
-    # Encode the raw text in base64 format for file download
-    b64 = base64.b64encode(raw_text.encode()).decode()
-
-    # Create a new filename with a timestamp
-    new_filename = "code_review_analysis_file_{}_.txt".format(time_str)
-
-    # Create an HTML link with the encoded content and filename for download
-    href = f'<a href="data:file/txt;base64,{b64}" download="{new_filename}">Click Here!!</a>'
-
-    return href
